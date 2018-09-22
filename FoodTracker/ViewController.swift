ef70804b7104ce69c7c9ca1187608d711a4d9aa4
Pratheeksha Ravindra Naik <prp412@udml9.local>
2018-09-22 12:30:12 -0600

    interface design completed

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
 
+    @IBOutlet weak var nameTextField: UITextField!   //Linking text-field to code
+    
+    @IBOutlet weak var mealNameLabel: UILabel!  //Connecting Label to code
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabeltext(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
     }
-
-
+    
+   
+    
 }
 
